0bb0707cbde9b7da1a1b9cf4e3c76462a2108be6
Sehajdeep Singh <sehajdeepsdsn@gmail.com>
2025-07-14 08:00:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+let your_Score = 0;
+let snorbo_Score = 0;
+
+function getSnorboChoice() {
+    const choices = ['rock', 'paper', 'scissors'];
+    return choices[Math.floor(Math.random() * 4)];
+}
+
+function game(playerChoice) {
+    const snorboChoice = getSnorboChoice();
+
+    let resultMessage = "";
+    let snorboImage = "assets/Snorbo_Happy.png";
+
+    if (playerChoice === snorboChoice) {
+        resultMessage = "It's a tie!";
+        snorboImage = "assets/Snorbo_Happy.png";
+    } 
+    else if (
+        (playerChoice === 'rock' && snorboChoice === 'scissors') ||
+        (playerChoice === 'paper' && snorboChoice === 'rock') ||
+        (playerChoice === 'scissors' && snorboChoice === 'paper')
+    ) {
+        your_Score++;
+        resultMessage = "Snorbo lost!";
+        snorboImage = "assets/Snorbo_Sad.png";
+    }
+    else {
+        snorbo_Score++;
+        resultMessage = "Snorbo won!";
+        snorboImage = "assets/Snorbo_Happy.png";
+    }
+
+    document.getElementById("player-score").textContent = your_Score;
+    document.getElementById("snorbo-score").textContent = snorbo_Score;
+
+    document.querySelector(".snorbo-img").src = snorboImage;
+    document.querySelector(".snorbo-status").textContent = resultMessage;
+}
